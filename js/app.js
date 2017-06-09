d46f142c803c6f409682f21f7f9c32ee79a9249a
James Moore <jameslmoore@gmail.com>
2017-06-08 21:34:35 -1000

    director takes button values and numbers

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 console.log("works");
+
+// var buttonValue = document.getElementById("resourceType");
+// console.log(buttonValue);
+
+var button = document.getElementById("requestResourceButton");
+button.addEventListener("click", director);
+
+//switch function based on buttonValue
+function director(){
+  var buttonValue = document.getElementById("resourceType").value;
+  //console.log(buttonValue);
+  var number = document.getElementById("resourceId").value;
+  //console.log(number);
+  if (number == 2){
+    console.log("twooooo");
+  }
+}
